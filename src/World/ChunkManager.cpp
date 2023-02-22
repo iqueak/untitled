d0cefa910f7c80e09b2b1608c53f0c599260aa9f
ique <iqueak@gmail.com>
2023-02-22 01:42:58 +0100

    fucking chunks fucking short int in indices

--- src/World/ChunkManager.cpp
+++ src/World/ChunkManager.cpp
@@ -3,11 +3,14 @@
 //
 #include "raylib.h"
 #include <iostream>
+#include <rlgl.h>
 #include "Block.h"
 #include "map"
 #include "ChunkManager.h"
 #include "WorldGenerator.h"
 #include "vector"
+
+#include "config.h"
 
 using namespace std;
 
@@ -21,71 +24,6 @@ namespace ChunkManager {
 
         return false;
     }
-
-    Mesh getCubeMesh() {
-
-        Mesh mesh = Mesh{0};
-
-        mesh.vertexCount = 24;
-        mesh.triangleCount = 12;
-
-        mesh.vertices = (float *) MemAlloc(24 * 3 * sizeof(float));
-        mesh.texcoords = (float *) MemAlloc(24 * 2 * sizeof(float));
-        mesh.normals = (float *) MemAlloc(24 * 3 * sizeof(float));
-        mesh.indices = (unsigned short *) RL_MALLOC(mesh.triangleCount * 3 * sizeof(unsigned short));
-
-        // iteration for Faces
-
-        int vCount = 0;
-        int tx = 0;
-        int n = 0;
-        int t = 0;
-        int w = 0;
-
-        for (int f = 0; f < 6; f++) {
-            //if (!checkneighbor(1, 1, f)) continue;
-
-            Block::BlockType& BlockData = Block::getBlockData(Block::BlockIDs::DIRT);
-
-
-            for (int v = 0; v < 4; v++) {
-                // vertices
-                mesh.vertices[w++] = cubeVertices[verticesForFace[f][v]].x;
-                mesh.vertices[w++] = cubeVertices[verticesForFace[f][v]].y;
-                mesh.vertices[w++] = cubeVertices[verticesForFace[f][v]].z;
-
-                // normals
-                mesh.normals[n++] = sideVectorForFace[f].x;
-                mesh.normals[n++] = sideVectorForFace[f].y;
-                mesh.normals[n++] = sideVectorForFace[f].z;
-
-                //texcoords
-                //mesh.texcoords[tx++] = texCoord[v]->x;
-                //mesh.texcoords[tx++] = texCoord[v]->y;
-
-                float x = BlockData.getTextureCoords(static_cast<Block::Faces>(f))[v].x;
-                float y = BlockData.getTextureCoords(static_cast<Block::Faces>(f))[v].y;
-
-                mesh.texcoords[tx++] = x;
-                mesh.texcoords[tx++] = y;
-            }
-
-            vCount += 4;
-
-            // triangles
-            mesh.indices[t++] = vCount - 4;
-            mesh.indices[t++] = vCount - 3;
-            mesh.indices[t++] = vCount - 2;
-            mesh.indices[t++] = vCount - 2;
-            mesh.indices[t++] = vCount - 1;
-            mesh.indices[t++] = vCount - 4;
-        }
-
-        UploadMesh(&mesh, false);
-        return mesh;
-    }
-
-
 
     void Init() {
         activeChunks = std::map<int,ChunkManager::Chunk>();
@@ -105,7 +43,7 @@ namespace ChunkManager {
 
     void buildMesh(Chunk &chunk) {
         vector<float> vertices;
-        vector<unsigned short> triangles;
+        vector<unsigned short> triangles;// TODO standart unsigned short
         vector<float> texcoords;
         vector<float> normals;
 
@@ -153,27 +91,20 @@ namespace ChunkManager {
             }
         }
 
-        //chunk.chunkMesh = Mesh{0};
-
         chunk.chunkMesh.vertexCount = vertices.size() / 3;
         chunk.chunkMesh.triangleCount = triangles.size() / 3;
 
-        chunk.chunkMesh.vertices = vertices.data();//(float *) MemAlloc(vertices.size() * sizeof(float));
-        chunk.chunkMesh.texcoords = texcoords.data();//(float *) MemAlloc(texcoords.size() * sizeof(float));
-        chunk.chunkMesh.normals = normals.data();//(float *) MemAlloc(normals.size() * sizeof(float));
+        chunk.chunkMesh.vertices = vertices.data();
+        chunk.chunkMesh.texcoords = texcoords.data();
+        chunk.chunkMesh.normals = normals.data();
 
-        /*** так как это short int то количество триугольников ограничено 32 тысячами
-         *   возможным решением будет не использование триугольников, или создание меша с помощью rlgl
-         *      https://github.com/raysan5/raylib/issues/807
-         *
-         *      попробовать класть триугольники в
-         *        mesh->   unsigned int vaoId;     // OpenGL Vertex Array Object id
-                  mesh->  unsigned int *vboId;
-         *      игнорируя short int для indices
-         *
+
+        /*** TODO так как indices это short int то количество триугольников ограничено 32 тысячами,
+         * из за того что максимальная вершина на которую может указать индекс, 65535, это 2730 блоков
+         *   нужно разделить чанк на секции, и хранить в секции меш, придумать какие значения брать для секции  и тд
          * ***/
-        chunk.chunkMesh.indices = triangles.data();//(unsigned short *) RL_MALLOC(triangles.size() * sizeof(unsigned short));
+        chunk.chunkMesh.indices = triangles.data();
 
-        UploadMesh(&chunk.chunkMesh, false);
+        UploadMesh(&chunk.chunkMesh,false);
     }
 }
--- src/World/WorldGenerator.cpp
+++ src/World/WorldGenerator.cpp
@@ -19,10 +19,19 @@ namespace WorldManager {
             for (int y = 0; y < chunkMaxHeight; y++) {
                 for (int x = 0; x < chunkSize; x++) {
                     for (int z = 0; z < chunkSize; z++) {
-                        //if (y == 0) setType = BlockIDs::BEDROCK;
-                        //if (y > 0) setType = BlockIDs::STONE;
-                        if (y > 10) setType = BlockIDs::DIRT;
-                        if (y > 16) setType = BlockIDs::AIR;
+                        if (y == 0) setType = BlockIDs::BEDROCK;
+                        if (y == 1) setType = BlockIDs::STONE;
+                        if (y == 2) setType = BlockIDs::BEDROCK;
+                        if (y == 3) setType = BlockIDs::STONE;
+                        if (y == 4) setType = BlockIDs::BEDROCK;
+                        if (y == 5) setType = BlockIDs::STONE;
+                        if (y == 6) setType = BlockIDs::BEDROCK;
+                        if (y == 7) setType = BlockIDs::STONE;
+                        if (y == 8) setType = BlockIDs::BEDROCK;
+                        if (y == 9) setType = BlockIDs::STONE;
+                        if (y == 10) setType = BlockIDs::BEDROCK;
+                        if (y == 11) setType = BlockIDs::STONE;
+                        if (y > 11) setType = BlockIDs::AIR;
                         chunk.chunkData[y][x][z] = setType;
                     }
                 }
